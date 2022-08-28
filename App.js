e31eb9048ad3fd393a816a654b8eae25c7749764
Pau Perez <saulopefa@gmail.com>
2022-08-28 15:00:13 +0200

    Store push token in the backend
    This ports https://github.com/coopdevs/timeoverflow-mobile-app/pull/1.

--- App.js
+++ App.js
@@ -90,12 +90,19 @@ export default function App() {
     }
   };
 
+  const handleLoggedInPage = (webview, token) => {
+    webview?.injectJavaScript(`window.TimeOverflowRegisterExpoDeviceToken(\'${token}\');`);
+  };
+
   const handleLoginPage = (webview) => {
     webview?.injectJavaScript("$('#user_remember_me').prop('checked', true);")
   };
 
   const injectCustomJavaScript = (url) => {
-    if (/sign_in|login/.test(url)) {
+    if (/members/.test(url)) {
+      handleLoggedInPage(webViewRef.current, expoPushToken);
+    }
+    else if (/sign_in|login/.test(url)) {
       handleLoginPage(webViewRef.current);
     }
   };
